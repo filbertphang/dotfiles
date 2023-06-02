860ff420fe5234ec4b9eece19e1bc7b83334f884
filbert <fillberth@gmail.com>
2023-06-02 16:38:41 +0800

    add custom vscode theme to dotfiles

--- setup.sh
+++ setup.sh
@@ -5,5 +5,6 @@ ln -s $PWD/.vimrc ~/.vimrc
 ln -s $PWD/.gitconfig ~/.gitconfig
 ln -s $PWD/.p10k.zsh ~/.p10k.zsh
 ln -s $PWD/custom-config.zsh ~/.oh-my-zsh/custom/custom-config.zsh
+ln -s $PWD/nordic-rose ~/.vscode-server/extensions/nordic-rose
 
 source ~/.zshrc
