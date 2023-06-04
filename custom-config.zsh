edd6622340ec397e4e00d5d5a774dab3a4232481
filbert <fillberth@gmail.com>
2023-06-04 17:23:22 +0800

    update theme

--- custom-config.zsh
+++ custom-config.zsh
@@ -17,11 +17,17 @@ alias vim="nvim"
 alias vv="vim ~/.vimrc"
 alias psql="sudo -u postgres psql"
 alias jks="bundle exec jekyll serve --force-polling"
+alias cat="batcat"
 
 # advent of code
 alias s1="g++ sol1.cpp -o sol1 && ./sol1 < input"
+alias t1="g++ sol1.cpp -o sol1 && ./sol1 < test"
 alias s2="g++ sol2.cpp -o sol2 && ./sol2 < input"
+alias t2="g++ sol2.cpp -o sol2 && ./sol2 < test"
 alias day='/mnt/c/Users/filbert/Documents/NUS/Year\ 2/aoc22/tool/setupDay.sh'
+
+# cs2103t
+alias duke="javac -d bin src/main/**/*.java && java -cp bin duke.Duke"
 
 # man page coloring
 export LESS_TERMCAP_mb=$'\e[1;32m'
@@ -42,3 +48,9 @@ ZSH_HIGHLIGHT_STYLES[comment]='fg=012'
 function termcolors() {
   for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'}; done
 }
+
+# run ssh-agent on boot
+if [[ -z $SSH_AGENT_SOCK ]]; then
+  eval "$(ssh-agent -s)" > /dev/null
+  ssh-add ~/.ssh/github_rsa 2> /dev/null
+fi
